19bcbd0a2c9163eb51a8a6d30cb8bb4e6296aa1f
Orel Balilti <orelba93@gmail.com>
2018-08-05 23:05:00 +0300

    export types

--- index.ts
+++ index.ts
@@ -4,4 +4,8 @@ export {ISocketHandler} from "./interfaces/socket-handler.interface";
 
 export {socketService} from "./services/socket.service";
 
-import "rxjs";
+import * as SocketIOStatic from "socket.io";
+import * as socketCookieParser from "socket.io-cookie";
+
+export {SocketIOStatic};
+export {socketCookieParser};
