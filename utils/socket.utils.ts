08750d3ba18fbf85913fb1cad16a1259cba53c4f
Orel Balilti <orelba93@gmail.com>
2018-12-05 23:02:27 +0200

    Schema validation support were added

--- utils/socket.utils.ts
+++ utils/socket.utils.ts
@@ -6,5 +6,5 @@ import {SocketError} from "../exceptions/socket.exception";
 export function BreakMiddleware();
 export function BreakMiddleware(reason:any);
 export function BreakMiddleware(reason?:any){
-    throw new SocketError(EXCEPTIONS.BREAK_MIDDLEWARE.message,EXCEPTIONS.BREAK_MIDDLEWARE.code,reason);
+    throw new SocketError(EXCEPTIONS.BREAK_MIDDLEWARE.message,EXCEPTIONS.BREAK_MIDDLEWARE.code,reason,false);
 }
