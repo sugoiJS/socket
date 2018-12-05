7c73c9795f70720cae98d69e2072529e56155db3
Orel Balilti <orelba93@gmail.com>
2018-12-05 21:07:34 +0200

    custom exception code added

--- decorators/schema-policy.decorator.ts
+++ decorators/schema-policy.decorator.ts
@@ -8,22 +8,24 @@ import {EXCEPTIONS} from "../constants/exceptions.constant";
  * @returns {(target: any, property: string, descriptor: PropertyDescriptor) => any}
  * @constructor
  */
-export function SocketSchemaPolicy(schema:TComparableSchema){
+export function SocketSchemaPolicy(schema: TComparableSchema);
+export function SocketSchemaPolicy(schema: TComparableSchema, exceptionErrorCode: number);
+export function SocketSchemaPolicy(schema: TComparableSchema, exceptionErrorCode: number = 400) {
     return function (target: any, property: string, descriptor: PropertyDescriptor) {
         const originalFunction = descriptor.value;
-        descriptor.value = new Proxy(originalFunction,{
-            apply:(applyTarget, thisArg, argArray) => {
-                validateSchema(schema,argArray[1]);
-                Reflect.apply(applyTarget,thisArg,argArray);
+        descriptor.value = new Proxy(originalFunction, {
+            apply: (applyTarget, thisArg, argArray) => {
+                validateSchema(schema, argArray[1], exceptionErrorCode);
+                Reflect.apply(applyTarget, thisArg, argArray);
             }
         });
-        Reflect.defineProperty(target,property,descriptor);
+        Reflect.defineProperty(target, property, descriptor);
     }
 }
 
-function validateSchema(schema,data){
-        const validationRslt = ValidateSchemaUtil.ValidateSchema(data,schema);
-        if(!validationRslt.valid){
-            throw new SocketError(EXCEPTIONS.POLICY_BLOCKED.message,EXCEPTIONS.POLICY_BLOCKED.code,validationRslt);
-        }
+function validateSchema(schema, data, exceptionErrorCode: number) {
+    const validationRslt = ValidateSchemaUtil.ValidateSchema(data, schema);
+    if (!validationRslt.valid) {
+        throw new SocketError(EXCEPTIONS.POLICY_BLOCKED.message, exceptionErrorCode || EXCEPTIONS.POLICY_BLOCKED.code, validationRslt);
+    }
 }
