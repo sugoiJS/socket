5c998a751ed23cd1b166243d315f20e345e509ed
Orel Balilti <orelba93@gmail.com>
2018-12-06 23:55:22 +0200

    remove js leftovers

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export { socketOn } from "./decorators/socket";
-export { ISocketHandler } from "./interfaces/socket-handler.interface";
-export { socketService } from "./services/socket.service";
-import "rxjs";
--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var socket_1 = require("./decorators/socket");
-exports.socketOn = socket_1.socketOn;
-var socket_service_1 = require("./services/socket.service");
-exports.socketService = socket_service_1.socketService;
-require("rxjs");
